57eee21c130d0e098e97e0b3f700529632732cd2
blurden <blurden@gmail.com>
2013-03-27 12:47:34 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basetouch::Application.config.secret_token = '99f7cd8d33c61561f8d18f375bc2d68725be89718e647527aa2f2072894c6e082e4b7612215a85d8e980b25912e658ecdc4dafc4deed54c6ca7b68d4b385424a'
